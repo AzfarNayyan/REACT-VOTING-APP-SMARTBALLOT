5f5e543193af8d580d105888fdbef59e81cb6a96
Azfar Nayyan <125149813+AzfarNayyan@users.noreply.github.com>
2024-05-02 00:03:11 +0500

    Add files via upload

---
+++ src/SignUp_Admin/AdminSignUp.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './AdminSignUp.css';
+import axios from 'axios';
+
+const AdminSignUp = () => {
+  const [message, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const cnic = form['cnic'].value;
+    const name = form['name'].value;
+    const email = form['email'].value;
+    const password = form['password'].value;
+
+    try {
+      // Check if admin account already exists
+      const existsResponse = await axios.post('http://localhost:5501/checkAdminExistence', { cnic, email });
+      if (existsResponse.data.exists) {
+        setMessage('Admin account with provided CNIC or email already exists.');
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+          setMessage('');
+        }, 5000); // Hide message after 5 seconds
+        return;
+      }
+
+      // If admin account does not exist, sign up
+      const signUpResponse = await axios.post('http://localhost:5501/adminSignUp', { cnic, name, email, password });
+      setMessage(signUpResponse.data);
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+        setMessage('');
+      }, 5000); // Hide message after 5 seconds
+    } catch (error) {
+      console.error('Error signing up admin:', error);
+      setMessage('Error signing up admin. Please try again later.');
+    }
+  };
+  
+  return (
+    <div className="signup-container">
+      <h1 className="title-signup">Welcome to SmartBallot</h1>
+      <div className="signup-box">
+        <h2>Admin Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="signup-form-group">
+            <label htmlFor="cnic">CNIC:</label>
+            <input type="text" id="cnic" name="cnic" />
+          </div>
+          <div className="signup-form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="signup-form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="signup-form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="submit" className="signup-btn">Sign Up</button>
+        </form>
+        {showMessage && (
+          <div className="message-popup">
+            <p>{message}</p>
+          </div>
+        )}
+        {showSuccessMessage && (
+          <div className="message-popup">
+            <p>{message}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AdminSignUp;
