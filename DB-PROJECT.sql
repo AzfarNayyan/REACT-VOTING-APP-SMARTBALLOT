e751985202f894bb6f86bf4c58982048068bbf14
Azfar Nayyan <125149813+AzfarNayyan@users.noreply.github.com>
2024-05-01 04:16:15 -0700

    Create DB-PROJECT.sql

---
+++ DB-PROJECT.sql
@@ -0,0 +1,79 @@
+create database DBProject
+go
+use DBProject
+go
+
+
+--DATABASE SCHEMA CONSISTING 7 TABLES.
+
+--VOTERS TABLE
+CREATE TABLE Voters (
+  VoterID INT IDENTITY(1,1) PRIMARY KEY NOT NULL, 
+  CNIC VARCHAR(20) UNIQUE NOT NULL, 
+  Username VARCHAR(255) NOT NULL,
+  Address VARCHAR(MAX),
+  Email VARCHAR(255) UNIQUE NOT NULL, 
+  [Password] VARCHAR(255) NOT NULL
+);
+
+
+--CONSTITUENCIES TABLE
+CREATE TABLE Constituencies (
+  ConstituencyName VARCHAR(255) NOT NULL,
+  ConstituencyID INT IDENTITY(1,1) PRIMARY KEY
+);
+
+--CANDIDATES TABLE
+CREATE TABLE Candidates (
+  Name VARCHAR(255) NOT NULL,
+  PartyAffiliation VARCHAR(255),
+  Bio TEXT,
+  CandidateID INT IDENTITY(1,1) PRIMARY KEY,
+  ConstituencyID INT NOT NULL,
+  FOREIGN KEY (ConstituencyID) REFERENCES Constituencies(ConstituencyID) ON DELETE CASCADE ON UPDATE CASCADE, 
+);
+
+--VOTES TABLE
+CREATE TABLE Votes (
+  TimeStamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+  VoteID INT IDENTITY(1,1) PRIMARY KEY,  
+  VoterID INT NOT NULL,
+  FOREIGN KEY (VoterID) REFERENCES Voters(VoterID) ON DELETE CASCADE ON UPDATE CASCADE, 
+  CandidateID INT NOT NULL,
+  FOREIGN KEY (CandidateID) REFERENCES Candidates(CandidateID) ON DELETE CASCADE ON UPDATE CASCADE, 
+);
+
+--ADMIN TABLE
+CREATE TABLE Admin (
+  CNIC VARCHAR(20) unique not null,
+  Name VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) unique not null,
+  [Password] VARCHAR(255) NOT NULL,
+  AdminID INT IDENTITY(1,1) PRIMARY KEY 
+);
+
+
+
+
+--FORM 47 TABLE
+CREATE TABLE Form47 (
+  Form47ID INT IDENTITY(1,1) PRIMARY KEY,
+  ConstituencyID INT NOT NULL,
+  FOREIGN KEY (ConstituencyID) REFERENCES Constituencies(ConstituencyID) ON DELETE CASCADE ON UPDATE CASCADE, 
+  ReturningOfficer VARCHAR(255),
+  TotalVotes INT,
+  ValidVotes INT,
+  InvalidVotes INT
+);
+
+--RESULT TABLE
+CREATE TABLE Result (
+  VotePercentages DECIMAL(10,2),  
+  CandidateID INT NOT NULL,
+  FOREIGN KEY (CandidateID) REFERENCES Candidates(CandidateID), 
+  ConstituencyID INT NOT NULL,
+  FOREIGN KEY (ConstituencyID) REFERENCES Constituencies(ConstituencyID), 
+);
+
+
+select * from Admin
